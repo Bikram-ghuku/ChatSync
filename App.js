a0c541018884cb69f44eb3c6c7ffb0d52946d668
Bikram Ghuku <bikramghuku05@gmail.com>
2023-12-18 20:21:14 +0530

    added login screene

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './Store';
+import Login from './screens/Login';
 
 export default function App() {
+	
   return (
 	<Provider store={store}>
-		<View style={styles.container}>
-		<Text>Open up App.js to start working on your app!</Text>
-		<StatusBar style="auto" />
-		</View>
+		<SafeAreaView style={styles.container}>
+			<Login/>
+		</SafeAreaView>
 	</Provider>
   );
 }
