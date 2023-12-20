5ac36b4f0124cc2d4fd10ed0e3b716cc7faf5b7d
Bikram Ghuku <bikramghuku05@gmail.com>
2023-12-20 21:34:58 +0530

    style: updated email icon

--- screens/Login.js
+++ screens/Login.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import Icon from 'react-native-vector-icons/FontAwesome'
+import Icon2 from 'react-native-vector-icons/Entypo'
 import { Image, Input } from 'react-native-elements'
 import { vw, vh, vmin, vmax } from 'react-native-expo-viewport-units';
 
@@ -12,8 +13,8 @@ const Login = () => {
                 <Image style={{width: 300, height: 300}} source={require('../assets/icon.png')} />
                 <Text style={{fontSize: 30, fontWeight: 900}}>Login</Text>
                 <View style={styles.inputs}>
-                    <Text style={styles.inputsText}>Username: </Text>
-                    <Input placeholder='Enter your username' style={styles.inputArea} leftIcon={<Icon name='user' size={24}/>}/>
+                    <Text style={styles.inputsText}>Email: </Text>
+                    <Input placeholder='Enter your username' style={styles.inputArea} leftIcon={<Icon2 name='mail' size={24}/>}/>
                 </View>
                 <View style={styles.inputs}>
                     <Text style={styles.inputsText}>Password: </Text>
