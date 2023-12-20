02c7cbbdcfb64fa4219b642cef4b15b781706f5a
Bikram Ghuku <bikramghuku05@gmail.com>
2023-12-20 21:16:25 +0530

    style: added homeScreen

--- App.js
+++ App.js
@@ -2,16 +2,26 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './Store';
 import Login from './screens/Login';
+import 'react-native-gesture-handler'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './screens/Home';
 
 export default function App() {
-	
-  return (
-	<Provider store={store}>
-		<SafeAreaView style={styles.container}>
-			<Login/>
-		</SafeAreaView>
-	</Provider>
-  );
+	const Stack = createNativeStackNavigator();
+	return (
+		<Provider store={store}>
+			<NavigationContainer>
+				<SafeAreaProvider>
+					<Stack.Navigator initialRouteName='Home'>
+						<Stack.Screen name='Home' component={Home} options={{headerShown: false}}/>
+						<Stack.Screen name='Login' component={Login} options={{headerShown: false}}/>
+					</Stack.Navigator>
+				</SafeAreaProvider>
+			</NavigationContainer>
+		</Provider>
+  	);
 }
 
 const styles = StyleSheet.create({
---
+++ screens/Home.js
@@ -0,0 +1,53 @@
+import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { vh, vw } from 'react-native-expo-viewport-units'
+
+const Home = () => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.imageContainer}>
+                <Image source={require('../assets/icon.png')} />
+            </View>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.button}><Text style={{color: '#003399'}}>Login</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.button}><Text style={{color: '#003399'}}>Signup</Text></TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default Home
+
+const styles = StyleSheet.create({
+    container:{
+        height:vh(100),
+        width:vw(100),
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    imageContainer:{
+        width: vw(100),
+        height: vh(90),
+        display:'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonContainer:{
+        display:'flex',
+        flexDirection:'row',
+        width:'100%',
+    },
+    button:{
+        fontWeight: 500,
+        height: vh(7),
+        width: '50%',
+        borderRadius: 10,
+        borderColor: '#003399',
+        borderCurve: 'solid',
+        borderWidth: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
