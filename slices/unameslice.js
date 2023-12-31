8e2a897fe75a8bc9f08a19d83c4092186c278fdb
Bikram Ghuku <bikramghuku05@gmail.com>
2023-12-31 17:19:08 +0530

    fix: updated unameslice to include jwt token

--- slices/unameslice.js
+++ slices/unameslice.js
@@ -4,16 +4,21 @@ export const unameSlice = createSlice({
     name: "uname",
     initialState: {
         value: null,
+        token: null,
     },
     reducers: {
         setUname: (state, action) => {
-            state.value = action.payload;
+            state.name = action.payload;
+        },
+        setToken: (state, action) => {
+            state.token = action.payload;
         },
     },
 });
 
-export const { setUname } = unameSlice.actions;
+export const { setUname, setToken } = unameSlice.actions;
 
-export const selectUname = (state) => state.uname.value;
+export const selectUname = (state) => state.uname.name;
+export const selectToken = (state) => state.uname.token;
 
 export default unameSlice.reducer;
